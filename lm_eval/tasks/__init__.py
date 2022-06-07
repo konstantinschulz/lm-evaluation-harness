9e1caabd7ae460d0d19105319a1e015cb715d06d
karina-hensel <henselkarina@gmail.com>
2022-06-07 13:56:10 +0200

    x_stance registered as task

--- lm_eval/tasks/__init__.py
+++ lm_eval/tasks/__init__.py
@@ -50,6 +50,7 @@ from . import blimp
 from . import asdiv
 from . import gsm8k
 from . import storycloze
+from . import x_stance
 
 ########################################
 # Translation tasks
@@ -283,6 +284,9 @@ TASK_REGISTRY = {
     # "storycloze_2016": storycloze.StoryCloze2016,
     # "storycloze_2018": storycloze.StoryCloze2018,
     # "sat": sat.SATAnalogies,
+
+    # X-Stance
+    "x_stance":x_stance.x_stance,
 }
 
 
