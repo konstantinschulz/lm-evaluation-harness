47a82f86904ba1f3f3dff9d7c1794333a5de6b1b
Konstantin Schulz <Konstantin.Schulz@dfki.de>
2022-06-09 11:07:42 +0200

    add run script for xnli_de

---
+++ run_xnli.py
@@ -0,0 +1,40 @@
+import json
+import torch.cuda
+from lm_eval import evaluator
+from lm_eval.models.gpt2 import HFLM
+
+
+def evaluate():
+    # device = "cuda"  # cuda:0
+    device = "cpu"
+    # hflm: HFLM = HFLM(pretrained="malteos/gpt2-wechsel-german-ds-meg", device=device)
+    hflm: HFLM = HFLM(pretrained="malteos/gpt2-xl-wechsel-german", device=device)
+    # dbmdz/german-gpt2 yongzx/gpt2-finetuned-oscar-de benjamin/gpt2-wechsel-german facebook/xglm-1.7B
+    # facebook/xglm-564M facebook/xglm-1.7B
+    results = evaluator.simple_evaluate(
+        model=hflm,
+        # model_args=args.model_args,
+        tasks=["xnli_de"],
+        num_fewshot=10,  # 10 25
+        batch_size=1,
+        device=device,
+        no_cache=True,
+        limit=10,
+        description_dict={},
+        # check_integrity=args.check_integrity
+    )
+    if hasattr(results["config"]["model"], "gpt2"):
+        results["config"]["model"] = results["config"]["model"].gpt2.config.name_or_path
+    dumped = json.dumps(results, indent=2)
+    print(dumped)
+    with open("output.json", "w") as f:
+        f.write(dumped)
+    # print(
+    #     f"{args.model} ({args.model_args}), limit: {args.limit}, provide_description: {args.provide_description}, "
+    #     f"num_fewshot: {args.num_fewshot}, batch_size: {args.batch_size}"
+    # )
+    print(evaluator.make_table(results))
+
+
+print("CUDA available: ", torch.cuda.is_available())
+evaluate()
