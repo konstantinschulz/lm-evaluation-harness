f083cfa528e9dbadd2ab33713b8b5b230918599b
karina-hensel <henselkarina@gmail.com>
2022-06-20 11:12:20 +0200

    Update x_stance.py

--- lm_eval/tasks/x_stance.py
+++ lm_eval/tasks/x_stance.py
@@ -80,18 +80,18 @@ class x_stance(Task):
             # named differently than the default `"test"`.
             return self.dataset["test"]
 
-    def _process_doc(self, doc):
+    #def _process_doc(self, doc):
         # Process (detokenize, strip, replace etc.) each individual `doc`
         # with this function. You can map this across the docs in each available
         # dataset split. See the TODOs in `train_docs`, `validation_docs`, and
         # `test_docs` for snippets.
         # Returns doc with gold label
-        return ("QUESTION: "+ doc["question"]+ "\n\n"+ "COMMENT: "+ doc["comment"]+ "\n\n"+ "LABEL: "+ doc["label"])
+        #return ("QUESTION: "+ doc["question"]+ "\n\n"+ "COMMENT: "+ doc["comment"]+ "\n\n"+ "LABEL: "+ doc["label"])
 
     def doc_to_text(self, doc):
         # TODO: Format the query prompt portion of the document example.
         # Query part consists of the question and comment part only (no label)
-        return ("QUESTION: "+ doc["question"]+ "\n\n"+ "COMMENT: "+ doc["comment"]+ "\n\n"+ "LABEL: ")
+        return ("QUESTION: "+ doc["question"]+ "\n\n"+ "COMMENT: "+ doc["comment"]+ "\n\n"+ "LABEL:")
 
     def doc_to_target(self, doc):
         # TODO: Fill in the `target` ("gold answer") variable.
