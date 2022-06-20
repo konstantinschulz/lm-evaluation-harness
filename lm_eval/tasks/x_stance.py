eb22ff2c1cbaf04f4f2e27e41becd5e7d81c27d4
karina-hensel <henselkarina@gmail.com>
2022-06-20 10:13:39 +0200

    Update x_stance.py

--- lm_eval/tasks/x_stance.py
+++ lm_eval/tasks/x_stance.py
@@ -91,7 +91,7 @@ class x_stance(Task):
     def doc_to_text(self, doc):
         # TODO: Format the query prompt portion of the document example.
         # Query part consists of the question and comment part only (no label)
-        return "QUESTION: "+ doc["question"]+ "\n"+ "COMMENT: "+ doc["comment"]+ "\n"+ "LABEL: "
+        return ("QUESTION: "+ doc["question"]+ "\n"+ "COMMENT: "+ doc["comment"]+ "\n"+ "LABEL: ")
 
     def doc_to_target(self, doc):
         # TODO: Fill in the `target` ("gold answer") variable.
