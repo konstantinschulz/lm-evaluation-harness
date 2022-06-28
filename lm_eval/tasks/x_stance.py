b6d5a7b3f3ae72d334a240f8f4c9a8f804163b21
karina-hensel <henselkarina@gmail.com>
2022-06-28 09:51:32 +0200

    Update x_stance.py

--- lm_eval/tasks/x_stance.py
+++ lm_eval/tasks/x_stance.py
@@ -38,7 +38,7 @@ def _xstance_agg(key, items):
 def _xstance_precision(y_true, y_pred):
     precision_metric = datasets.load_metric("precision")
 
-    return precision_metric.compute(references=y_true, predictions=y_pred)
+    return precision_metric.compute(references=y_true, predictions=y_pred, average=None)
 
 
 class x_stance(Task):
