5526a56ce480001bf3253a960e2f4968b2beb3d5
karina-hensel <henselkarina@gmail.com>
2022-06-29 09:21:22 +0200

    Added recall

--- lm_eval/tasks/x_stance.py
+++ lm_eval/tasks/x_stance.py
@@ -34,13 +34,15 @@ _CITATION = """@inproceedings{vamvas2020xstance,
 # Helper functions for aggregation (adapted from SQUAD script)
 def _xstance_agg(key, items):
     references, predictions = zip(*items)
-    return _xstance_precision(references, predictions)[key]
+    return [_xstance_precision(references, predictions)[key], _xstance_precision(references, predictions)[key]]
 
 def _xstance_precision(y_true, y_pred):
     precision_metric = datasets.load_metric("precision")
+    return precision_metric.compute(references=y_true, predictions=y_pred, average='macro', labels=np.unique(y_pred))
+
+def _xstance_recall(y_true, y_pred):
     recall_metric = datasets.load_metric("recall")
-    return [precision_metric.compute(references=y_true, predictions=y_pred, average='macro', labels=np.unique(y_pred)),
-            recall_metric.compute(references=y_true, predictions=y_pred, average='macro', labels=np.unique(y_pred))]
+    return recall_metric.compute(references=y_true, predictions=y_pred, average='macro', labels=np.unique(y_pred))
 
 
 class x_stance(Task):
