b375ad24284fdd82721c74f825de70b44e26214c
karina-hensel <henselkarina@gmail.com>
2022-07-06 09:35:03 +0200

    Class names corrected

--- lm_eval/tasks/x_stance.py
+++ lm_eval/tasks/x_stance.py
@@ -10,7 +10,7 @@ The data have been extracted from the Swiss voting advice platform Smartvote.
 
 https://github.com/ZurichNLP/xstance
 """
-
+import datasets
 from datasets import Dataset
 from lm_eval.base import Task, rf
 #import lm_eval.datasets.x_stance.x_stance
