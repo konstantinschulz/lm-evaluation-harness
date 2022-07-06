ea1542bfd378e5069ed217c8931a423d082743d1
karina-hensel <henselkarina@gmail.com>
2022-07-06 12:48:49 +0200

    Changed prompts to German

--- lm_eval/tasks/x_stance.py
+++ lm_eval/tasks/x_stance.py
@@ -76,8 +76,8 @@ class XStance(Task):
         
     def doc_to_text(self, doc):
         #return "QUESTION: "+ doc["question"]+ "\n\n"+ "COMMENT: "+ doc["comment"]+ "\n\n"+ "LABEL: "
-        return "question: "+ doc["question"]+ "\n\n"+ "comment: "+ doc["comment"]+ "\n\n"+ "label: "
-        #return "Frage: "+ doc["question"]+ "\n\n"+ "Kommentar: "+ doc["comment"]+ "\n\n"+ "Kategorie: "
+        #return "question: "+ doc["question"]+ "\n\n"+ "comment: "+ doc["comment"]+ "\n\n"+ "label: "
+        return "Frage: "+ doc["question"]+ "\n\n"+ "Kommentar: "+ doc["comment"]+ "\n\n"+ "Label: "
 
     def doc_to_target(self, doc):
         target = doc["label"]
