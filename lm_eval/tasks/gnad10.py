680c364dcf2e86f47ab5826563767e6c47fee1bf
karina-hensel <henselkarina@gmail.com>
2022-07-13 10:18:04 +0200

    Update gnad10.py

--- lm_eval/tasks/gnad10.py
+++ lm_eval/tasks/gnad10.py
@@ -92,7 +92,7 @@ class GNAD10(Task):
           
     def _process_doc(self, doc):
       # Truncate documents which exceed the maximum token length (1024) of the model
-      if len(doc['text']) > 1024:
+      if len(doc['text'].split(' ')) > 1024:
         tmp = " "
         for t in doc['text'].split(' ')[:1023]:
           tmp += t + ' '
